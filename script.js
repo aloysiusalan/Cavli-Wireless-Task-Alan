29f53a500adf86c26f36dae17507f4919c1c8409
aloysiusalan <74831108+aloysiusalan@users.noreply.github.com>
2023-12-17 19:03:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let popup= document.getElementById("popupbox");
+
+
+function OpenPopUp(){
+    popup.classList.add("popup-active");
+}
+
+
+
+function ClosePopUp() {
+    popup.classList.remove("popup-active");
+}
